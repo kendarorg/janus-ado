31ee8248b194903f2bd2cd5ccdcefd45ef1db072
Kendar <unknown@kendar.org>
2023-01-31 10:50:18 +0100

    Splitted project

---
+++ ado/PgWireCoreClient/PgWireCoreClient/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data;
+using System.Data.Common;
+
+DbProviderFactories.RegisterFactory("Npgsql", Npgsql.NpgsqlFactory.Instance);
+
+using (DataTable providers = DbProviderFactories.GetFactoryClasses())
+{
+    Console.WriteLine("Available ADO.NET Data Providers:");
+    foreach (DataRow prov in providers.Rows)
+    {
+        Console.WriteLine("Name:{0}", prov["Name"]);
+        Console.WriteLine("Description:{0}", prov["Description"]);
+        Console.WriteLine("Invariant Name:{0}", prov["InvariantName"]);
+    }
+}
+
+DbProviderFactory factory = DbProviderFactories.GetFactory("Npgsql");
+DbConnection conn = factory.CreateConnection();
+conn.ConnectionString = "Host=localhost; Database=test; Username=sa;Password=sa;SSL Mode=DISABLE;";
+conn.Open();
+var cmd = conn.CreateCommand();
+cmd.CommandText = "create table if not exists test(id int, name varchar)";
+cmd.ExecuteNonQuery();
+
+cmd = conn.CreateCommand();
+cmd.CommandText = "insert into test values(1,'fuffa')";
+cmd.ExecuteNonQuery();
+
+cmd = conn.CreateCommand();
+cmd.CommandText = "insert into test values(2,'boffa')";
+cmd.ExecuteNonQuery();
+
+cmd = conn.CreateCommand();
+cmd.CommandText = "select * FROM test";
+var reader = cmd.ExecuteReader();
+if (reader.HasRows)
+{
+    while (reader.Read())
+    {
+        Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+            reader.GetString(1));
+    }
+}
