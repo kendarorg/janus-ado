197ba46719f649e7896f5b32d07dc1613b33ea75
Kendar <unknown@kendar.org>
2023-01-31 13:19:12 +0100

    Starting ado provider

---
+++ ado/PgWireCoreClient/PgWireAdo/wire/server/StartupMessage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using PgWireAdo.wire.client;
+
+namespace PgWireAdo.wire.server
+{
+    public class StartupMessage : PGServerMessage
+    {
+        private readonly Dictionary<string, string> _parameters;
+        readonly Dictionary<string, string> _serverParameters = new ();
+
+        public StartupMessage(Dictionary<string, string> parameters)
+        {
+            _parameters = parameters;
+        }
+
+        public IDictionary<string, string> ServerParameters => _serverParameters;
+
+        public void Write(NetworkStream stream)
+        {
+            //SEND THE MESSAGE PLUS PARAMETERS
+            var authenticationOk = new AuthenticationOk();
+            if (authenticationOk.IsMatching(stream))
+            {
+                authenticationOk.Read(stream);
+                var backendKeyData = new BackendKeyData();
+                if (backendKeyData.IsMatching(stream))
+                {
+                    backendKeyData.Read(stream);
+                    var parameterStatus = new ParameterStatus();
+                    while (parameterStatus.IsMatching(stream))
+                    {
+                        parameterStatus.Read(stream);
+                        _serverParameters.Add(parameterStatus.Key,parameterStatus.Value);
+                    }
+
+                    var readyForQuery = new ReadyForQuery();
+                    if (readyForQuery.IsMatching(stream))
+                    {
+                        readyForQuery.Read(stream);
+                    }
+                }
+            }
+        }
+    }
+}
