a5533120638210b0360c273dc9ce33fa2261ed05
Kendar <unknown@kendar.org>
2023-03-03 14:44:42 +0100

    Added simple verification for ODBC

---
+++ ado/PgWireCoreClient/ODBCConnection/AllTestsSetup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ODBCConnection
+{
+    [SetUpFixture]
+    public class AllTestsSetup 
+    {
+        private Process _process;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+            Trace.Flush();
+        }
+        
+    }
+}
